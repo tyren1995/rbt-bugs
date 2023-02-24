959b56b0eec7d0d1271ad9135e23953f16849a60
Tyren Rhinehart <trhinehart@bp-3.com>
2023-02-24 10:38:15 -0700

    add relevant code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typeahead } from 'react-bootstrap-typeahead';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 function App() {
+  //Delete the @ts-ignore and you'll see error:
+  //'Option' refers to a value, but is being used as a type here. Did you mean 'typeof Option'?
+  //@ts-ignore
+  const [options, setOptions] = React.useState<Option[] | undefined>();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width:'50vw'}}>
+      <Typeahead
+        options={["a","b","c"]}
+        // this clearButton is double-imposed
+        clearButton
+    />
     </div>
   );
 }
